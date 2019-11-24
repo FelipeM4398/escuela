7397cd01cd23fa144457ac95065b29253c1a6384
FelipeM4398 <luisf4398@hotmail.com>
2019-11-23 21:28:48 -0500

    Add travis in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDbVDnC6bgVmMyJ-wI9OAwHeAPg34ti_VU",
+    authDomain: "angularjs-c0b29.firebaseapp.com",
+    databaseURL: "https://angularjs-c0b29.firebaseio.com",
+    projectId: "angularjs-c0b29",
+    storageBucket: "angularjs-c0b29.appspot.com",
+    messagingSenderId: "440755331511",
+    appId: "1:440755331511:web:3a9a8e984ae0711ea6c832"
+  }
 };
